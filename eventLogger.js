7bb6de80925a8153d49335607aa4ad4d23bf6ce9
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-03-20 17:39:53 +0530

    ..

--- eventLogger.js
+++ eventLogger.js
@@ -5,9 +5,13 @@ const path = require("path");
 class Logger extends EventEmitter {
   log(message) {
     const logMessage = `${new Date().toString()} - ${message}\r\n`; //log msg added into log.txt file
-    console.log(logMessage);
-
-    fs.appendFileSync(path.join(__dirname, "log.txt"), logMessage); //create log.txt file
+    // console.log(logMessage);
+    process.stdout.write(logMessage);
+    try {
+      fs.appendFileSync(path.join(__dirname, "log.txt"), logMessage); //create log.txt file
+    } catch (err) {
+      console.log("Error adding file");
+    }
   }
 }
 
