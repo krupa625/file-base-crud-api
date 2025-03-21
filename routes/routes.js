2150906b8ac5a68e1bcae7d708a4eab5b644df0c
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-03-21 13:53:02 +0530

    create routes

---
+++ routes/routes.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const path = require("path");
+
+const route = (req, res) => {
+  console.log("URL:", req.url);
+  console.log("Method:", req.method);
+
+  const getData = require("../Controllers/getData");
+  const postData = require("../Controllers/postData");
+  const putData = require("../Controllers/putData");
+  const deleteData = require("../Controllers/deleteData");
+  const getDataId = require("../Controllers/getDataId");
+
+  if (req.method === "GET" && req.url === "/") {
+    //serving static file here
+    try {
+      fs.readFile(
+        path.join(__filename, "public", "index.html"),
+        (err, data) => {
+          if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("404 Not Found");
+          } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+          }
+        }
+      );
+    } catch (err) {
+      res.end(err, { message: "Something went wrong!" });
+    }
+  } else if (req.method === "GET" && req.url === "/api/data") {
+    getData(req, res);
+  } else if (req.method === "GET" && req.url === "/api/data") {
+    getData(req, res);
+  } else if (req.method === "GET" && req.url.startsWith("/api/data/")) {
+    getDataId(req, res);
+  } else if (req.method === "POST" && req.url === "/api/data") {
+    postData(req, res);
+  } else if (req.method === "PUT" && req.url.startsWith("/api/data/")) {
+    putData(req, res);
+  } else if (req.method === "DELETE" && req.url.startsWith("/api/data/")) {
+    deleteData(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+};
+
+module.exports = route;
--- server.js
+++ server.js
@@ -1,47 +1,9 @@
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const handleroutes = require("./routes/routes");
 
-const getData = require("./routes/getData");
-const postData = require("./routes/postData");
-const putData = require("./routes/putData");
-const deleteData = require("./routes/deleteData");
-const getDataId = require("./routes/getDataId");
 const server = http.createServer((req, res) => {
-  console.log("Method:", req.method);
-  console.log("URL:", req.url);
-
-  if (req.method === "GET" && req.url === "/") {
-    //serving static file here
-    try {
-      fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
-        if (err) {
-          res.writeHead(404, { "Content-Type": "text/plain" });
-          res.end("404 Not Found");
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(data);
-        }
-      });
-    } catch (err) {
-      res.end(err, { message: "Something went wrong!" });
-    }
-  } else if (req.method === "GET" && req.url === "/api/data") {
-    getData(req, res);
-  } else if (req.method === "GET" && req.url.startsWith("/api/data/")) {
-    getDataId(req, res);
-  } else if (req.method === "POST" && req.url === "/api/data") {
-    postData(req, res);
-  } else if (req.method === "PUT" && req.url.startsWith("/api/data/")) {
-    putData(req, res);
-  } else if (req.method === "DELETE" && req.url.startsWith("/api/data/")) {
-    deleteData(req, res);
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Route not found" }));
-  }
+  handleroutes(req, res);
 });
-
 server.listen(3030, () => {
   console.log("Server running at http://localhost:3030");
 });
